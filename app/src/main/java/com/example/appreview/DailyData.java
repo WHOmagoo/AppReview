50ff5132c1f37d12112cbccb862208efd962257e
hmcgough <hrmagoo@gmail.com>
2020-01-05 23:13:54 -0800

    Implement data storage and observer pattern

--- app/src/main/java/com/example/appreview/DailyData.java
+++ app/src/main/java/com/example/appreview/DailyData.java
@@ -1,26 +1,99 @@
 package com.example.appreview;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.Date;
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+import java.util.Observable;
+import java.util.Scanner;
 
-public class DailyData {
+public class DailyData extends Observable {
     private int happinessLevelBefore;
     private int happinessLevelAfter;
     private int anxietyLevelBefore;
     private int anxietyLevelAfter;
+
+    public Date getDate() {
+        return date;
+    }
+
     private Date date;
     private int dayNum;
-    
+    private static DateFormat dateFormat = new SimpleDateFormat("yy/MM/dd");
+
     public DailyData(Date date, int dayNum){
         this.date = date;
         this.dayNum = dayNum;
+        happinessLevelBefore = -1;
+        happinessLevelAfter = -1;
+        anxietyLevelBefore = -1;
+        anxietyLevelAfter = -1;
     }
-    
+
+    public static DailyData makeDailyDataFromCSV(String s){
+        Scanner scanner = new Scanner(s);
+        scanner.useDelimiter(",");
+        return makeDailyDataFromCSV(scanner);
+    }
+
+    public static DailyData makeDailyDataFromCSV(Scanner scanner) {
+        Date date = null;
+        try {
+            date = dateFormat.parse(scanner.next());
+        } catch (ParseException e) {
+            try {
+                //This shouldn't fail unless the dateFormat changes and this doesn't get updated
+                date = dateFormat.parse("20/01/01");
+            } catch (ParseException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        int dayNum = -1;
+        try {
+            dayNum = scanner.nextInt();
+        } catch (InputMismatchException ignored){}
+
+        DailyData result = new DailyData(date, dayNum);
+
+        try {
+            try {
+                result.happinessLevelBefore = scanner.nextInt();
+            } catch (InputMismatchException ignored) {
+            }
+
+            try {
+                result.happinessLevelAfter = scanner.nextInt();
+            } catch (InputMismatchException ignored) {
+            }
+
+            try {
+                result.anxietyLevelBefore = scanner.nextInt();
+            } catch (InputMismatchException ignored) {
+            }
+
+            try {
+                result.anxietyLevelAfter = scanner.nextInt();
+            } catch (InputMismatchException ignored) {
+            }
+        } catch (NoSuchElementException ignored){}
+
+        return result;
+    }
+
     public boolean setHappinessLevelBefore(int level){
         if(intOutOfBounds(level)){
             return false;
         }
-        
+
         happinessLevelBefore = level;
+        if(!intOutOfBounds(anxietyLevelBefore)){
+            setChanged();
+            notifyObservers(false);
+        }
         return true;
     }
 
@@ -30,6 +103,11 @@ public class DailyData {
         }
 
         happinessLevelAfter = level;
+
+        if(!intOutOfBounds(anxietyLevelAfter)){
+            setChanged();
+            notifyObservers(true);
+        }
         return true;
     }
 
@@ -39,6 +117,11 @@ public class DailyData {
         }
 
         anxietyLevelBefore = level;
+
+        if(!intOutOfBounds(happinessLevelBefore)){
+            setChanged();
+            notifyObservers(false);
+        }
         return true;
     }
 
@@ -48,28 +131,82 @@ public class DailyData {
         }
 
         anxietyLevelAfter = level;
+
+        if(!intOutOfBounds(happinessLevelAfter)){
+            setChanged();
+            notifyObservers(true);
+        }
         return true;
     }
-    
+
     private boolean intOutOfBounds(int level){
-        return level < 0 | level > 10;   
+        return level < 0 | level > 10;
     }
 
-    public String makeCsv(){
+    public static String getCSVHeadings(){
         StringBuilder sb = new StringBuilder();
-        sb.append(date);
+        sb.append("Date,");
+        sb.append("Day Number,");
+        sb.append("Happiness Level Before,");
+        sb.append("Happiness Level After,");
+        sb.append("Anxiety Level Before,");
+        sb.append("Anxiety Level After,");
+
+        return new String(sb);
+    }
+
+    public String makeCSV(){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(dateFormat.format(date));
         sb.append(",");
         sb.append(dayNum);
         sb.append(",");
-        sb.append(happinessLevelBefore);
+        if(!intOutOfBounds(happinessLevelBefore)) {
+            sb.append(happinessLevelBefore);
+        }
         sb.append(",");
-        sb.append(happinessLevelAfter);
+        if(!intOutOfBounds(happinessLevelAfter)) {
+            sb.append(happinessLevelAfter);
+        }
         sb.append(",");
-        sb.append(anxietyLevelBefore);
+
+        if(!intOutOfBounds(anxietyLevelBefore)) {
+            sb.append(anxietyLevelBefore);
+        }
         sb.append(",");
-        sb.append(anxietyLevelAfter);
-        sb.append("\n");
+        if(!intOutOfBounds(anxietyLevelAfter)) {
+            sb.append(anxietyLevelAfter);
+        }
 
         return new String(sb);
+    }
+
+    public int getHappinessLevelBefore() {
+        return happinessLevelBefore;
+    }
+
+    public int getHappinessLevelAfter() {
+        return happinessLevelAfter;
+    }
+
+    public int getAnxietyLevelBefore() {
+        return anxietyLevelBefore;
+    }
+
+    public int getAnxietyLevelAfter() {
+        return anxietyLevelAfter;
+    }
+
+    public int getDayNum() {
+        return dayNum;
+    }
+
+    public boolean appUsed(){
+        return !intOutOfBounds(happinessLevelBefore) && !intOutOfBounds(anxietyLevelBefore);
+    }
+
+    public boolean taskFinished(){
+        return !intOutOfBounds(happinessLevelAfter) && !intOutOfBounds(anxietyLevelAfter);
     }
 }
--- app/src/main/java/com/example/appreview/UserData.java
+++ app/src/main/java/com/example/appreview/UserData.java
@@ -1,51 +1,182 @@
 package com.example.appreview;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
-public class UserData {
-    private static final UserData ourInstance = new UserData();
-    int curDay;
+public class UserData extends Observable implements Observer {
+    private static final UserData ourInstance = new UserData(MainActivity.getAppContext());
+    private int curDay;
 
-    ArrayList<DailyData> data;
-    DailyData curData;
+    private ArrayList<DailyData> data;
+    private DailyData curData;
+    private String identifier;
+
+    SharedPreferences userDataSaved;
 
     public static UserData getInstance() {
         return ourInstance;
     }
 
-    private UserData() {
-        data = new ArrayList<>();
+    private UserData(Context context) {
+        data = new ArrayList<DailyData>();
+
+        boolean created = false;
+
+        userDataSaved = context.getSharedPreferences("data", 0);
+        int count = userDataSaved.getInt("count", 0);
+
+        identifier = userDataSaved.getString("id", "J Doe");
+
+
+        for(int i = 1; i <= count; i++){
+            String curDayData = userDataSaved.getString("day" + i, "20/01/01,"+ i + ",,,,");
+            curData = DailyData.makeDailyDataFromCSV(curDayData);
+            data.add(curData);
+        }
+
+        curDay = count;
+
+        if(count == 0){
+            nextDay();
+        } else {
+            curData.addObserver(this);
+        }
+    }
+
+//    /****
+//     * If ourInstance has already been initialized, will not make new instance
+//     * @param context the android context
+//     * @return ourInstance
+//     */
+//    public static UserData makeInstance(Context context){
+//        return ourInstance;
+//    }
+
+    public void setIdentifier(String id){
+        identifier = id;
     }
 
     public int getCurDay(){
         return curDay;
     }
 
+    public String getIdentifier(){
+        return identifier;
+    }
+
     /***
      *
-     * @return The data that represents today, could be null
+     * @return The data that represents today, never is null
      */
     public DailyData getCurrentData(){
         return curData;
     }
 
     public DailyData nextDay(){
+        if(curData != null){
+            curData.deleteObserver(this);
+        }
         curDay++;
-        DailyData todaysData = new DailyData(Calendar.getInstance().getTime(), curDay);
+        Date curDate = Calendar.getInstance().getTime();
+        DailyData todaysData = new DailyData(curDate, curDay);
         data.add(todaysData);
         curData = todaysData;
+
+        curData.addObserver(this);
+
+        setChanged();
+        notifyObservers();
+
+        writeDay();
+
         return todaysData;
     }
 
-    public void SkipDay(){
-        curDay++;
-        data.add(null);
-        curData = null;
+    public static String makeCSVHeading(){
+        return "Identifier," + DailyData.getCSVHeadings();
     }
 
     public String makeCSV(){
-        //TODO finish this.
-        return "";
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(makeCSVHeading());
+        sb.append("\n");
+
+        for (int i = 0; i < data.size(); i++) {
+            sb.append(identifier);
+            sb.append(",");
+            sb.append(curData.makeCSV());
+            sb.append("\n");
+        }
+
+        return new String(sb);
+    }
+
+    private void makeUserDataFromCSV(String csv){
+        Scanner scanner = new Scanner(csv);
+        makeUserDataFromCSV(scanner);
+    }
+
+    private void makeUserDataFromCSV(Scanner scanner){
+        scanner.useDelimiter(",");
+        scanner.nextLine();
+        while(scanner.hasNext()){
+            identifier = scanner.next();
+            data.add(DailyData.makeDailyDataFromCSV(scanner));
+        }
+
+        if(data.size() > 0) {
+            DailyData lastEntry = data.get(data.size() - 1);
+            Date startingDate = lastEntry.getDate();
+            int lastDayNum = lastEntry.getDayNum();
+
+            Date currentDate = Calendar.getInstance().getTime();
+
+            float daysBetween = TimeUnit.DAYS.convert(currentDate.getTime() - startingDate.getTime(), TimeUnit.MILLISECONDS);
+            for(; lastDayNum <= daysBetween; lastDayNum++){
+                nextDay();
+            }
+        } else {
+            nextDay();
+        }
+    }
+
+    public boolean hasAppUsed() {
+        return curData.appUsed();
+    }
+
+    public boolean hasTaskFinished(){
+        return curData.taskFinished();
+    }
+
+    private void writeDay(){
+        SharedPreferences.Editor editor = userDataSaved.edit();
+
+        editor.putString("day" + curDay, curData.makeCSV());
+        editor.putInt("count", curDay);
+        editor.apply();
+    }
+
+    @Override
+    public void update(Observable observable, Object o) {
+        if(observable instanceof DailyData){
+            writeDay();
+
+            setChanged();
+            notifyObservers();
+        }
+    }
+
+    @Override
+    public void notifyObservers(){
+        super.notifyObservers();
     }
 }
