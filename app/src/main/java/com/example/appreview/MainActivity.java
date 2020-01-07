82991ce05fe741bf705cc02d4c1a9fd9334660e3
hmcgough <hrmagoo@gmail.com>
2020-01-06 21:25:56 -0800

    Add automatic push notifications, share button and copy button for csv data

--- app/src/main/java/com/example/appreview/MainActivity.java
+++ app/src/main/java/com/example/appreview/MainActivity.java
@@ -1,20 +1,32 @@
 package com.example.appreview;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.core.content.FileProvider;
 
 import android.content.SharedPreferences;
 
+import android.os.SystemClock;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,15 +35,14 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.net.URI;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.Scanner;
 
-import static androidx.core.content.FileProvider.getUriForFile;
-
 public class MainActivity extends AppCompatActivity implements Observer {
     private static Context context;
+    public static final String CHANNEL_ID = "All notifications";
+    public static final int NOTIFICATION_ID = 23;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +64,59 @@ public class MainActivity extends AppCompatActivity implements Observer {
             editor.apply();
         }
 
+        AlarmManager alarmMgr;
+        PendingIntent alarmIntent;
+
+        alarmMgr = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, SampleBootReceiver.class);
+        alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+
+        //TODO reenable inexact repetition for alarm after testing
+        alarmMgr.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                SystemClock.elapsedRealtime() +
+                        30 * 1000,60000, alarmIntent);
+
+//        alarmMgr.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+//                SystemClock.elapsedRealtime() +
+//                        30 * 1000,30 * 1000, alarmIntent);
+
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Daily Reminder";
+            String description = "This channel will go off once daily";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    public void sendPushNotification(){
+        createNotificationChannel();
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID);
+        builder.setContentTitle("Don't forget to review the app today!");
+        builder.setSmallIcon(R.drawable.review_icon_foreground);
+        builder.setPriority(Notification.PRIORITY_DEFAULT);
+        Intent notifyIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 2, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        //to be able to launch your activity from the notification
+        builder.setContentIntent(pendingIntent);
+
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+        managerCompat.notify(NOTIFICATION_ID, builder.build());
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        updateView();
     }
 
     @Override
@@ -74,27 +138,18 @@ public class MainActivity extends AppCompatActivity implements Observer {
             Toast.makeText(super.getApplicationContext(), "Settings have not been implemented", Toast.LENGTH_SHORT).show();
 
             return true;
-        }
-
-        if(id == R.id.action_increment_day){
+        } else if(id == R.id.action_increment_day){
             UserData.getInstance().nextDay();
             return true;
-        }
-
-
-        if(id == R.id.action_spoof_review_first){
+        } else if(id == R.id.action_spoof_review_first){
             DailyData d = UserData.getInstance().getCurrentData();
             d.setAnxietyLevelBefore((int) (Math.random() * 11));
             d.setHappinessLevelBefore((int) (Math.random() * 11));
-        }
-
-        if(id == R.id.action_spoof_review_second){
+        } else if(id == R.id.action_spoof_review_second){
             DailyData d = UserData.getInstance().getCurrentData();
             d.setAnxietyLevelAfter((int) (Math.random() * 11));
             d.setHappinessLevelAfter((int) (Math.random() * 11));
-        }
-
-        if(id == R.id.action_export_csv){
+        } else if(id == R.id.action_share){
             String generatedCSVData = UserData.getInstance().makeCSV();
 
             FileOutputStream fOut = null;
@@ -144,10 +199,19 @@ public class MainActivity extends AppCompatActivity implements Observer {
                 sharingIntent.putExtra(Intent.EXTRA_STREAM, contentUri);
 //                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "CSV Data!");
 //                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, "Extra text");
-                startActivity(Intent.createChooser(sharingIntent, "Share via"));
+                startActivity(Intent.createChooser(sharingIntent, "Share CSV file via"));
             }catch (IOException e) {
                     e.printStackTrace();
                 }
+        } else if (id == R.id.copy_data){
+            ClipboardManager myClipboard;
+            myClipboard = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
+            String csvString = UserData.getInstance().makeCSV();
+            ClipData csvClipData = ClipData.newPlainText("text", csvString);
+            assert myClipboard != null;
+            myClipboard.setPrimaryClip(csvClipData);
+        } else if (id == R.id.action_send_push_notification){
+            sendPushNotification();
         }
 
         //noinspection SimplifiableIfStatement
@@ -195,8 +259,29 @@ public class MainActivity extends AppCompatActivity implements Observer {
 //        updateDayCounter();
     }
 
+    private void updateDailyProgress(){
+        ImageView firstReviewStatus = findViewById(R.id.firstReviewStatus);
+        ImageView appStatus = findViewById(R.id.appStatus);
+        ImageView secondReviewStatus = findViewById(R.id.secondReviewStatus);
+
+
+        if(UserData.getInstance().hasAppUsed()) {
+            firstReviewStatus.setImageResource(R.mipmap.review_icon);
+
+            appStatus.setImageResource(R.mipmap.video_game_icon_completed);
+            if (UserData.getInstance().hasTaskFinished()){
+                secondReviewStatus.setImageResource(R.mipmap.review_icon);
+            }
+        } else {
+            firstReviewStatus.setImageResource(R.mipmap.review_pending_icon);
+            secondReviewStatus.setImageResource(R.mipmap.review_pending_icon);
+            appStatus.setImageResource(R.mipmap.video_game_pending_icon);
+        }
+    }
+
     private void updateView(){
         updateDayCounter();
+        updateDailyProgress();
         setReviewedToday(UserData.getInstance().hasAppUsed());
         setDayFinished(UserData.getInstance().hasTaskFinished());
 
