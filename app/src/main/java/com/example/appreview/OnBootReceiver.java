4c5b7bdf56ce406f0203fa9958a490ead37223b5
hmcgough <hrmagoo@gmail.com>
2020-01-06 22:19:25 -0800

    Add boot receiver

---
+++ app/src/main/java/com/example/appreview/OnBootReceiver.java
@@ -0,0 +1,12 @@
+package com.example.appreview;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class OnBootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        DailyNotification dn = new DailyNotification(context, 0);
+    }
+}
